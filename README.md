9b4ef874e03aa5605baac0ff5dfc27fd4c9eda95
Aizaz Khaja <aizazkj@gmail.com>
2018-09-18 22:15:04 -0400

    added some scaffolding for the app. Not finished yet, but it's a start.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # realtzy
-A basic realtor app (for demonstration purposes only)
+
+A basic realtor app (for demonstration purposes only).
+
+## Feature list
+
+- User Authentication
+- Index of property listings
+- Basic CRM
---
+++ routes.js
@@ -0,0 +1,18 @@
+
+
+
+
+// GET /index
+// show all real estate listings in a table
+
+// GET /crm
+// show all client info in a table
+
+// POST /crm/new
+// add one new client to the table
+
+// PATCH /crm/:client_id/update
+// update information on a client from table.
+
+// DELETE /crm/:client_id/delete
+// delete information on a client from table.
