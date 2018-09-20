697235fa373b6d6b57050384b11f882725151417
Aizaz Khaja <aizazkj@gmail.com>
2018-09-20 05:10:07 -0400

    added Client model and controller routes. Also created files for listings, but is empty.

---
+++ controllers/listingsController.js
@@ -0,0 +1,1 @@
+// TODO: create routes to display all listings.
--- models/Client.js
+++ models/Client.js
@@ -2,5 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const clientSchema = new Schema({
-  
+  userId: Schema.Types.ObjectId,
+  firstName: String,
+  lastName: String,
+  type: String,
+  date: {type: Date, default: Date.now}
 });
+
+const Client = mongoose.model('Client', clientSchema);
+
+module.exports = {model: Client, schema: clientSchema};
